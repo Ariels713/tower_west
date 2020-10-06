d2fbd0de20aeed7d0ac91dcbf70a5bc87c0cba20
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-10-05 22:00:07 -0400

    get function created

---
+++ functions/getGallaryPhotos.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const Airtable = require("airtable");
+
+Airtable.configure({
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+const table = base.table("gallery");
+
+exports.handler = async (event) => {
+  const records = await table.select({}).firstPage();
+  const formattedData = records.map((record) => ({
+    id: record.id,
+    fields: record.fields,
+  }));
+  return {
+    statusCode: 200,
+    body: JSON.stringify(formattedData),
+  };
+};
